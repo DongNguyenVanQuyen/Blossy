b1d64f344b402daa98e594b38246862f9aec5a1c
DongNguyenVanQuyen <dongnguyenvanquyen@gmail.com>
2025-09-09 16:13:12 +0700

    update file html to php

---
+++ User/Footer.php
@@ -0,0 +1,66 @@
+ <footer id="Footer">
+      <div id="footer-wrapper">
+        <div class="footer-container">
+          <div class="footer-brand">
+            <div class="footer-logo">
+              <span>B</span>
+              <img src="../Seasons/Logo/Xuan.png" alt="logo" />
+              <span>SSY</span>
+            </div>
+            <p>
+              Những bông hoa tươi thắm trao gửi yêu thương <br />
+              Hãy trân trọng những khoảnh khắc cuộc sống với những bông hoa
+              tươi<br />
+              Được hái thủ công
+            </p>
+            <div class="footer-socials">
+              <i class="fa-brands fa-facebook-f"></i>
+              <i class="fa-brands fa-twitter"></i>
+              <i class="fa-brands fa-instagram"></i>
+              <i class="fa-brands fa-tiktok"></i>
+            </div>
+          </div>
+
+          <div class="footer-links">
+            <div class="footer-col">
+              <h4>Khám Phá</h4>
+              <a href="#">Về Chúng Tôi</a>
+              <a href="#">Cửa Hàng</a>
+              <a href="#">Tuyển Dụng</a>
+            </div>
+            <div class="footer-col">
+              <h4>Hỗ Trợ</h4>
+              <a href="#">Tài Khoản</a>
+              <a href="#">Theo Dõi Đơn Hàng</a>
+              <a href="#">Mua && Trả Hàng</a>
+              <a href="#">FAQs</a>
+            </div>
+            <div class="footer-col">
+              <h4>Legal</h4>
+              <a href="#">Chính Sách</a>
+              <a href="#">Điều Khoản && Điều Kiện</a>
+              <a href="#">Chính Sách Trả Hàng</a>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div class="footer-bottom">
+          <p>
+            Copyright © 2025 <span>Flower Shop Website</span>. All Rights
+            Reserved.
+          </p>
+          <div class="footer-lang-currency">
+            <select name="language">
+              <option>Tiếng Việt</option>
+              <option>English</option>
+            </select>
+            <select name="currency">
+              <option>VND</option>
+              <option>USD</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </footer>
---
+++ User/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- font -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap"
+      rel="stylesheet"
+    />
+    <!-- icon -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+      integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <link rel="stylesheet" href="../Assets/Css/reset.css" />
+    <link rel="stylesheet" href="../Assets/Css/style.css" />
+    <title>Blossy</title>
+  </head>
+  <body>
+    <?php include_once 'Header.php'; ?>
+
+    <!-- 2 -->
+    <div id="Main" class="pos-rel">
+      <div id="Time" class="pos-fx"></div>
+    </div>
+
+    <!-- 3 -->
+    <?php include_once 'Footer.php'; ?>
+  </body>
+  <script src="../Assets/Js/Script.js"></script>
+</html>
